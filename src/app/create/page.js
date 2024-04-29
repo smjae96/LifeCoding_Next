ad68f0311777b8a14a7cc67cc44b012763e1dace
smjae96 <tlawodbs0528@naver.com>
2024-04-29 22:09:32 +0900

    0429 생활코딩 Nextjs

---
+++ src/app/create/page.js
@@ -0,0 +1,7 @@
+export default function Create() {
+    return (
+        <>Create!!</>
+    )
+}
+// 만약 create 폴더에 layout.js가 있으면 그것과 이 page.js 파일을 결합시키나, 없는 경우
+// 부모 폴더로 가서 layout.js가 있는지 찾아서 그것과 이 page.js 파일을 결합시킴.
